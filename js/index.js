9a5fd5c706c05fb5651201cbef19a525f892fa16
Emil Folino BTH MAC <emilfolino@gmail.com>
2018-02-05 12:50:00 +0100

    Finished for example

--- js/index.js
+++ js/index.js
@@ -1,4 +1,19 @@
-var m = require("mithril");
-var list = require("./views/list.js");
+"use strict";
 
-m.mount(document.body, list);
+var m = require("mithril");
+var layout = require("./views/layout");
+var list = require("./views/list");
+var year = require("./views/year");
+
+m.route(document.body, "/", {
+    "/": {
+        render: function() {
+            return m(layout, m(list));
+        }
+    },
+    "/year/:year": {
+        render: function(vnode) {
+            return m(layout, m(year, vnode.attrs));
+        }
+    }
+});
--- js/views/list.js
+++ js/views/list.js
@@ -3,9 +3,22 @@ var m = require("mithril");
 
 module.exports = {
     view: function() {
-        return m("main.container", [
+        var startYear = 2010;
+        var endYear = 2017;
+        var years = [];
+
+        while (startYear <= endYear) {
+            years.push(
+                m("a.button.blue-button.full-width-button",
+                { href: "/year/" + startYear, oncreate: m.route.link },
+                startYear));
+            startYear++;
+        }
+
+        return [
             m("h1", "Nobelfesten"),
-            m("p", "Välj ett årtal i listan:")
-        ]);
+            m("p", "Välj ett årtal i listan:"),
+            m("div.year-container", years)
+        ];
     }
 };
